445b538da15c0a87e4fdf714fc6167913c25c952
wallleap <15579576761@163.com>
2024-04-30 11:52:40 +0800

    update

---
+++ assets/Post-C6kNTDO-.js
@@ -0,0 +1,181 @@
+import{d as S,v as _,r as p,K as C,z as T,a as A,P as D,Q as O,R as q,n as E,f as L,i as s,j as e,t as r,m as i,A as m,F as u,k as f,b,S as N,p as l,u as V,T as z}from"./index-BsnFrl1m.js";import{_ as x}from"./MarkdownIt.vue_vue_type_script_setup_true_lang-kDekkczl.js";const B=`pre code.hljs {
+  display: block;
+  overflow-x: auto;
+  padding: 1em
+}
+code.hljs {
+  padding: 3px 5px
+}
+/*
+
+Atom One Dark by Daniel Gamage
+Original One Dark Syntax theme from https://github.com/atom/one-dark-syntax
+
+base:    #282c34
+mono-1:  #abb2bf
+mono-2:  #818896
+mono-3:  #5c6370
+hue-1:   #56b6c2
+hue-2:   #61aeee
+hue-3:   #c678dd
+hue-4:   #98c379
+hue-5:   #e06c75
+hue-5-2: #be5046
+hue-6:   #d19a66
+hue-6-2: #e6c07b
+
+*/
+.hljs {
+  color: #abb2bf;
+  background: #282c34
+}
+.hljs-comment,
+.hljs-quote {
+  color: #5c6370;
+  font-style: italic
+}
+.hljs-doctag,
+.hljs-keyword,
+.hljs-formula {
+  color: #c678dd
+}
+.hljs-section,
+.hljs-name,
+.hljs-selector-tag,
+.hljs-deletion,
+.hljs-subst {
+  color: #e06c75
+}
+.hljs-literal {
+  color: #56b6c2
+}
+.hljs-string,
+.hljs-regexp,
+.hljs-addition,
+.hljs-attribute,
+.hljs-meta .hljs-string {
+  color: #98c379
+}
+.hljs-attr,
+.hljs-variable,
+.hljs-template-variable,
+.hljs-type,
+.hljs-selector-class,
+.hljs-selector-attr,
+.hljs-selector-pseudo,
+.hljs-number {
+  color: #d19a66
+}
+.hljs-symbol,
+.hljs-bullet,
+.hljs-link,
+.hljs-meta,
+.hljs-selector-id,
+.hljs-title {
+  color: #61aeee
+}
+.hljs-built_in,
+.hljs-title.class_,
+.hljs-class .hljs-title {
+  color: #e6c07b
+}
+.hljs-emphasis {
+  font-style: italic
+}
+.hljs-strong {
+  font-weight: bold
+}
+.hljs-link {
+  text-decoration: underline
+}
+`,P=`pre code.hljs {
+  display: block;
+  overflow-x: auto;
+  padding: 1em
+}
+code.hljs {
+  padding: 3px 5px
+}
+/*
+
+Atom One Light by Daniel Gamage
+Original One Light Syntax theme from https://github.com/atom/one-light-syntax
+
+base:    #fafafa
+mono-1:  #383a42
+mono-2:  #686b77
+mono-3:  #a0a1a7
+hue-1:   #0184bb
+hue-2:   #4078f2
+hue-3:   #a626a4
+hue-4:   #50a14f
+hue-5:   #e45649
+hue-5-2: #c91243
+hue-6:   #986801
+hue-6-2: #c18401
+
+*/
+.hljs {
+  color: #383a42;
+  background: #fafafa
+}
+.hljs-comment,
+.hljs-quote {
+  color: #a0a1a7;
+  font-style: italic
+}
+.hljs-doctag,
+.hljs-keyword,
+.hljs-formula {
+  color: #a626a4
+}
+.hljs-section,
+.hljs-name,
+.hljs-selector-tag,
+.hljs-deletion,
+.hljs-subst {
+  color: #e45649
+}
+.hljs-literal {
+  color: #0184bb
+}
+.hljs-string,
+.hljs-regexp,
+.hljs-addition,
+.hljs-attribute,
+.hljs-meta .hljs-string {
+  color: #50a14f
+}
+.hljs-attr,
+.hljs-variable,
+.hljs-template-variable,
+.hljs-type,
+.hljs-selector-class,
+.hljs-selector-attr,
+.hljs-selector-pseudo,
+.hljs-number {
+  color: #986801
+}
+.hljs-symbol,
+.hljs-bullet,
+.hljs-link,
+.hljs-meta,
+.hljs-selector-id,
+.hljs-title {
+  color: #4078f2
+}
+.hljs-built_in,
+.hljs-title.class_,
+.hljs-class .hljs-title {
+  color: #c18401
+}
+.hljs-emphasis {
+  font-style: italic
+}
+.hljs-strong {
+  font-weight: bold
+}
+.hljs-link {
+  text-decoration: underline
+}
+`,I={key:0,class:"min-h-60vh text-gray-600 slide-enter-1000 dark:text-gray-400"},R={class:"text-gray-800 dark:text-gray-300"},U={class:"mb-10 flex flex-wrap items-center gap-2 text-gray-400"},$={key:0},F=e("i",{class:"fa-regular fa-pen-to-square mr-1 text-gray-300 dark:text-gray-600"},null,-1),G={key:1},M=e("i",{class:"fa-regular fa-calendar mr-1 text-gray-300 dark:text-gray-600"},null,-1),H=e("span",null,"·",-1),K=e("i",{class:"fa-solid fa-hashtag mr-0.6 font-size-3 text-gray-300 dark:text-gray-600"},null,-1),Q={key:0},J=e("h2",{class:"text-gray-700 dark:text-gray-300"}," 评论 ",-1),X={class:"text-gray-400"},Y={class:"flex items-center line-height-none"},Z=["src"],W={class:"m-l-2"},ee={class:"m-0"},ne={class:"m-0 mt-1 font-size-3.4 text-gray-400"},te={class:"mt-3"},se={key:1,class:"text-gray-400"},le={class:"text-gray-400"},ae=["href"],oe=e("i",{class:"fa-solid fa-link mr-1 text-gray-300 dark:text-gray-700 group-hover:c-primary"},null,-1),re={key:1,class:"animate-blink animate-iteration-infinite text-center font-size-4 text-gray-400"},de=S({__name:"Post",setup(ce){const n=_({id:1,title:"",times:1,summary:"",comments:0,comments_url:"",body:"",date:"",updated:"",labels:[],milestone:{id:1,title:""},num:1}),c=_([]),h=N(),j=p(""),g=p("light"),k=C();function y(a){const o=document.getElementById(decodeURIComponent(a));o&&o.scrollIntoView({behavior:"instant",block:"start"})}function v(){const a=document.querySelector("#toc-wrapper"),o=a==null?void 0:a.querySelectorAll("a");o&&o.forEach(t=>{t.addEventListener("click",w=>{w.preventDefault();const d=t.getAttribute("href")||"";y(d==null?void 0:d.slice(1))})})}return T(async()=>{g.value=k.curTheme;const a=g.value==="light"?P:B,o=document.querySelector("#dynamic-style");o==null||o.remove();const t=document.createElement("style");t.innerHTML=a,t.id="dynamic-style",document.head.appendChild(t)}),A(async()=>{try{Object.assign(n,await D({number:Number(h.params.num)})),O(n),n.title&&(document.title=`${n.title} - wallleap`),j.value=n.comments_url.replace("comments","").replace("api.","").replace("repos/",""),n.comments>0&&c.push(...await q({url:n.comments_url})),await E(),h.hash&&n.body&&y(h.hash.slice(1)),v()}catch(a){console.error(a)}}),L(()=>{document.title="wallleap"}),(a,o)=>n.title?(l(),s("article",I,[e("h1",R,r(n.title),1),e("div",U,[n.date?(l(),s("span",$,[e("time",null,[F,i(r(n.date),1)])])):m("",!0),n.date!==n.updated?(l(),s("span",G,[e("time",null,[M,i(r(n.updated),1)])])):m("",!0),H,(l(!0),s(u,null,f(n.labels,t=>(l(),s("span",{key:t.id},[K,i(r(t.name),1)]))),128))]),b(x,{content:n.body,"render-toc":!0},null,8,["content"]),n.body?(l(),s("div",Q,[J,c.length>0?(l(),s(u,{key:0},[e("p",X," 共有 "+r(c.length)+" 条评论 ",1),(l(!0),s(u,null,f(c,t=>(l(),s("div",{key:t.id,class:"mb-6"},[e("div",Y,[e("img",{class:"h-10 w-10 border-rd",src:t.user.avatar_url,alt:"avatar"},null,8,Z),e("div",W,[e("h3",ee,r(t.user.login),1),e("p",ne,r(V(z)(t.created_at)),1)])]),e("div",te,[b(x,{content:t.body},null,8,["content"])])]))),128))],64)):(l(),s("p",se," 暂时没有评论 ")),e("p",le,[e("a",{class:"group",href:j.value,target:"_blank"},[oe,i("前往 Issue 页面评论")],8,ae)])])):m("",!0)])):(l(),s("p",re," 文章加载中…… "))}});export{de as default};
