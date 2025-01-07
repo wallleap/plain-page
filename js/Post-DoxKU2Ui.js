3944da9674ad8cde1e308e82eb528b5c979b3703
wallleap <15579576761@163.com>
2025-01-07 15:25:54 +0800

    update

---
+++ js/Post-DoxKU2Ui.js
@@ -0,0 +1,181 @@
+import{d as O,v as k,r as v,K as T,z as D,a as B,P as R,Q as V,R as N,n as U,f as z,i as a,j as t,t as u,m as g,A as y,F as b,k as w,b as S,S as P,T as H,p as r,u as I,U as $}from"./index-CFltwn1q.js";import{_ as C}from"./MarkdownIt.vue_vue_type_script_setup_true_lang-BJpQSP4O.js";import"./markdown-CGQCjh6-.js";import"./markdown-it-DHWoV2lB.js";import"./markdown-chunk-B5lr28AT.js";const F=`pre code.hljs {
+  display: block;
+  overflow-x: auto;
+  padding: 1em
+}
+code.hljs {
+  padding: 3px 5px
+}
+/*
+
+Atom One Dark by Daniel Gamage
+Original One Dark Syntax theme from https://github.com/atom/one-dark-syntax
+
+base:    #282c34
+mono-1:  #abb2bf
+mono-2:  #818896
+mono-3:  #5c6370
+hue-1:   #56b6c2
+hue-2:   #61aeee
+hue-3:   #c678dd
+hue-4:   #98c379
+hue-5:   #e06c75
+hue-5-2: #be5046
+hue-6:   #d19a66
+hue-6-2: #e6c07b
+
+*/
+.hljs {
+  color: #abb2bf;
+  background: #282c34
+}
+.hljs-comment,
+.hljs-quote {
+  color: #5c6370;
+  font-style: italic
+}
+.hljs-doctag,
+.hljs-keyword,
+.hljs-formula {
+  color: #c678dd
+}
+.hljs-section,
+.hljs-name,
+.hljs-selector-tag,
+.hljs-deletion,
+.hljs-subst {
+  color: #e06c75
+}
+.hljs-literal {
+  color: #56b6c2
+}
+.hljs-string,
+.hljs-regexp,
+.hljs-addition,
+.hljs-attribute,
+.hljs-meta .hljs-string {
+  color: #98c379
+}
+.hljs-attr,
+.hljs-variable,
+.hljs-template-variable,
+.hljs-type,
+.hljs-selector-class,
+.hljs-selector-attr,
+.hljs-selector-pseudo,
+.hljs-number {
+  color: #d19a66
+}
+.hljs-symbol,
+.hljs-bullet,
+.hljs-link,
+.hljs-meta,
+.hljs-selector-id,
+.hljs-title {
+  color: #61aeee
+}
+.hljs-built_in,
+.hljs-title.class_,
+.hljs-class .hljs-title {
+  color: #e6c07b
+}
+.hljs-emphasis {
+  font-style: italic
+}
+.hljs-strong {
+  font-weight: bold
+}
+.hljs-link {
+  text-decoration: underline
+}
+`,G=`pre code.hljs {
+  display: block;
+  overflow-x: auto;
+  padding: 1em
+}
+code.hljs {
+  padding: 3px 5px
+}
+/*
+
+Atom One Light by Daniel Gamage
+Original One Light Syntax theme from https://github.com/atom/one-light-syntax
+
+base:    #fafafa
+mono-1:  #383a42
+mono-2:  #686b77
+mono-3:  #a0a1a7
+hue-1:   #0184bb
+hue-2:   #4078f2
+hue-3:   #a626a4
+hue-4:   #50a14f
+hue-5:   #e45649
+hue-5-2: #c91243
+hue-6:   #986801
+hue-6-2: #c18401
+
+*/
+.hljs {
+  color: #383a42;
+  background: #fafafa
+}
+.hljs-comment,
+.hljs-quote {
+  color: #a0a1a7;
+  font-style: italic
+}
+.hljs-doctag,
+.hljs-keyword,
+.hljs-formula {
+  color: #a626a4
+}
+.hljs-section,
+.hljs-name,
+.hljs-selector-tag,
+.hljs-deletion,
+.hljs-subst {
+  color: #e45649
+}
+.hljs-literal {
+  color: #0184bb
+}
+.hljs-string,
+.hljs-regexp,
+.hljs-addition,
+.hljs-attribute,
+.hljs-meta .hljs-string {
+  color: #50a14f
+}
+.hljs-attr,
+.hljs-variable,
+.hljs-template-variable,
+.hljs-type,
+.hljs-selector-class,
+.hljs-selector-attr,
+.hljs-selector-pseudo,
+.hljs-number {
+  color: #986801
+}
+.hljs-symbol,
+.hljs-bullet,
+.hljs-link,
+.hljs-meta,
+.hljs-selector-id,
+.hljs-title {
+  color: #4078f2
+}
+.hljs-built_in,
+.hljs-title.class_,
+.hljs-class .hljs-title {
+  color: #c18401
+}
+.hljs-emphasis {
+  font-style: italic
+}
+.hljs-strong {
+  font-weight: bold
+}
+.hljs-link {
+  text-decoration: underline
+}
+`,M={key:0,class:"min-h-60vh text-gray-600 slide-enter-1000 dark:text-gray-400"},K={class:"text-gray-800 dark:text-gray-300"},Q={class:"mb-10 flex flex-wrap items-center gap-2 text-gray-400"},W={key:0},J=t("i",{class:"fa-regular fa-pen-to-square mr-1 text-gray-300 dark:text-gray-600"},null,-1),X={key:1},Y=t("i",{class:"fa-regular fa-calendar mr-1 text-gray-300 dark:text-gray-600"},null,-1),Z=t("span",null,"·",-1),ee=t("i",{class:"fa-solid fa-hashtag mr-0.6 font-size-3 text-gray-300 dark:text-gray-600"},null,-1),te={key:0},ne=t("h2",{class:"text-gray-700 dark:text-gray-300"}," 评论 ",-1),se={class:"text-gray-400"},le={class:"flex items-center line-height-none"},oe=["src"],ae={class:"m-l-2"},re={class:"m-0"},ce={class:"m-0 mt-1 font-size-3.4 text-gray-400"},ie={class:"mt-3"},he={key:1,class:"text-gray-400"},de={class:"text-gray-400"},ue=["href"],me=t("i",{class:"fa-solid fa-link mr-1 text-gray-300 dark:text-gray-700 group-hover:c-primary"},null,-1),pe={key:1,class:"animate-blink animate-iteration-infinite text-center font-size-4 text-gray-400"},xe=O({__name:"Post",setup(ge){const n=k({id:1,title:"",times:1,summary:"",comments:0,comments_url:"",body:"",date:"",updated:"",labels:[],milestone:{id:1,title:""},num:1}),p=k([]),j=P(),_=v(""),x=v("light"),E=T();function f(i){const s=document.getElementById(decodeURIComponent(i));s&&s.scrollIntoView({behavior:"instant",block:"start"})}function m(i){const s=document.querySelector("#toc-wrapper"),e=s==null?void 0:s.querySelectorAll("a");e&&(e.forEach(l=>{l.classList.remove("active")}),i.classList.add("active"))}function L(){const s=document.querySelector("#toc-wrapper").querySelectorAll("a"),e=[];s&&s.forEach(l=>{const h=l.getAttribute("href")||"",c=document.getElementById(decodeURIComponent(h==null?void 0:h.slice(1)));c&&e.push(c),location.hash===l.getAttribute("href")?(m(l),f(h==null?void 0:h.slice(1))):m(s[0]),l.addEventListener("click",o=>{o.preventDefault(),m(l);const d=l.getAttribute("href")||"";f(d==null?void 0:d.slice(1))})}),window.addEventListener("scroll",H(()=>{const l=[0,300],h=document.documentElement.clientHeight,c=e.map(o=>o.getBoundingClientRect());for(let o=0;o<e.length;o++){const d=c[o],q=d.top>=l[0]&&d.top<=l[1],A=d.top<l[0]&&c[o+1]&&(c[o+1].top>=h||c[o+1].top>=l[1]);if(q||A){m(s[o]);break}if(c[0].top>=h||c[0].top>=l[1]){m(s[0]);break}if(c[e.length-1].top<=l[0]){m(s[e.length-1]);break}}},100))}return D(async()=>{x.value=E.curTheme;const i=x.value==="light"?G:F,s=document.querySelector("#dynamic-style");s==null||s.remove();const e=document.createElement("style");e.innerHTML=i,e.id="dynamic-style",document.head.appendChild(e)}),B(async()=>{try{Object.assign(n,await R({number:Number(j.params.num)})),V(n),n.title&&(document.title=`${n.title} - wallleap`),_.value=n.comments_url.replace("comments","").replace("api.","").replace("repos/",""),n.comments>0&&p.push(...await N({url:n.comments_url})),await U(),j.hash&&n.body&&f(j.hash.slice(1)),L()}catch(i){console.error(i)}}),z(()=>{document.title="wallleap"}),(i,s)=>n.title?(r(),a("article",M,[t("h1",K,u(n.title),1),t("div",Q,[n.date?(r(),a("span",W,[t("time",null,[J,g(u(n.date),1)])])):y("",!0),n.date!==n.updated?(r(),a("span",X,[t("time",null,[Y,g(u(n.updated),1)])])):y("",!0),Z,(r(!0),a(b,null,w(n.labels,e=>(r(),a("span",{key:e.id},[ee,g(u(e.name),1)]))),128))]),S(C,{content:n.body,"render-toc":!0},null,8,["content"]),n.body?(r(),a("div",te,[ne,p.length>0?(r(),a(b,{key:0},[t("p",se," 共有 "+u(p.length)+" 条评论 ",1),(r(!0),a(b,null,w(p,e=>(r(),a("div",{key:e.id,class:"mb-6"},[t("div",le,[t("img",{class:"h-10 w-10 border-rd",src:e.user.avatar_url,alt:"avatar"},null,8,oe),t("div",ae,[t("h3",re,u(e.user.login),1),t("p",ce,u(I($)(e.created_at)),1)])]),t("div",ie,[S(C,{content:e.body},null,8,["content"])])]))),128))],64)):(r(),a("p",he," 暂时没有评论 ")),t("p",de,[t("a",{class:"group",href:_.value,target:"_blank"},[me,g("前往 Issue 页面评论")],8,ue)])])):y("",!0)])):(r(),a("p",pe," 文章加载中…… "))}});export{xe as default};
